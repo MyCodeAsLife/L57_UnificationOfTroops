81fbf931c976177d3a63b896fb310b3de93ccc85
Vladimir <w112vovan@mail.ru>
2023-12-26 15:50:46 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L57_UnificationOfTroops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Headquarters headquarters = new Headquarters();
+
+            headquarters.ShowAllPersonnels();
+        }
+    }
+
+    class Headquarters
+    {
+        private List<Soldier> _firstSquad = new List<Soldier>();
+        private List<Soldier> _secondSquad = new List<Soldier>();
+
+        public Headquarters()
+        {
+            Fill();
+        }
+
+        public void ShowAllPersonnels()
+        {
+            int maxNameLenght = _firstSquad.Max(soldier => soldier.Name.Length);
+            int maxRankLenght = _firstSquad.Max(soldier => soldier.Rank.Length);
+
+            Console.WriteLine("До перемещения:\nПервый отряд:");
+            Show(_firstSquad, maxNameLenght, maxRankLenght);
+            Console.WriteLine("\nВторой отряд:");
+            Show(_secondSquad, maxNameLenght, maxRankLenght);
+            Console.WriteLine("--------------------------------------");
+
+            TransferSoldiers();
+            Console.WriteLine("\nПосле перемещения:\nПервый отряд:");
+            Show(_firstSquad, maxNameLenght, maxRankLenght);
+            Console.WriteLine("\nВторой отряд:");
+            Show(_secondSquad, maxNameLenght, maxRankLenght);
+        }
+
+        private void Show(List<Soldier> squad, int maxNameLenght, int maxRankLenght)
+        {
+            foreach (var soldier in squad)
+                Console.WriteLine($"{{0, -{maxNameLenght}}} - {{1, -{maxRankLenght}}}", soldier.Name, soldier.Rank);
+        }
+
+        private void TransferSoldiers()
+        {
+            var sortSquad = _firstSquad.Where(soldier => soldier.Name.ToLower().StartsWith("б")).ToList();
+
+            foreach (var soldier in sortSquad)
+            {
+                _firstSquad.Remove(soldier);
+                _secondSquad.Add(soldier);
+            }
+        }
+
+        private void Fill()
+        {
+            _firstSquad.Add(new Soldier("Виталий", "Абакан", "Cтарший лейтенант", 36));
+            _firstSquad.Add(new Soldier("Александр", "Люгер", "Майор", 48));
+            _firstSquad.Add(new Soldier("Бенджамин", "Доллар", "Батя", 48));
+            _firstSquad.Add(new Soldier("Рембо", "Мачете", "Засекречено", 99));
+            _firstSquad.Add(new Soldier("Дмитрий", "ПКМ", "Подполковник", 68));
+            _firstSquad.Add(new Soldier("Петр", "АК-74М", "Рядовой", 7));
+            _firstSquad.Add(new Soldier("Борис Бритва", "Кольт М1911 А1", "Хрен попадеш", 98));
+
+            _secondSquad.Add(new Soldier("Витек", "АК-74М", "Рядовой", 3));
+            _secondSquad.Add(new Soldier("Саня", "АК-74М", "Рядовой", 5));
+            _secondSquad.Add(new Soldier("Игорь", "АК-74М", "Рядовой", 1));
+        }
+    }
+
+    class Soldier
+    {
+        public Soldier(string name, string weapon, string rank, int service)
+        {
+            Name = name;
+            Weapon = weapon;
+            Rank = rank;
+            Service = service;
+        }
+
+        public string Name { get; private set; }
+        public string Weapon { get; private set; }
+        public string Rank { get; private set; }
+        public int Service { get; private set; }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("L57_UnificationOfTroops")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L57_UnificationOfTroops")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("06c3822d-9b4c-4591-a5ed-17499fc9b909")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
